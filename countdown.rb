334ff77531de39a9cee5186fe7d8bd1559db1d2c
Mac Rowe <m.rowe42@gmail.com>
2021-02-23 18:07:01 -0500

    commit tree 2b62f2e33f53fb71c1223fe77c10b1f500224cd7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+counter = 10
 
-def countdown
+def countdown(counter)
+    while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    end
+    return "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(counter)
+    while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+    end
+end
+
